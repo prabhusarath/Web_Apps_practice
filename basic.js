f35cd9514dda21ad40861aba88d3fec8c6cf0e88
prabhusarath <prabhu.sarath@gmail.com>
2017-04-30 13:59:19 -0500

    JS Basics

---
+++ basic.js
@@ -0,0 +1,6 @@
+var f = prompt("Your First Name Please");
+var l = prompt("Your Last Name Please");
+var a = prompt("Your Age Please");
+
+console.log("your full name is "+f+ l);
+console.log("your are " + a + " years old");
