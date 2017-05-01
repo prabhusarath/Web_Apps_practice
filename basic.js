f64e48883f711298593948f16d117e03acb94ba5
prabhusarath <prabhu.sarath@gmail.com>
2017-04-30 20:53:45 -0500

    JS Basics

--- basic.js
+++ basic.js
@@ -6,12 +6,26 @@
 // console.log("your are " + a + " years old");
 
 
-var v =5
+// var v =5
 
-while(v<50)
-{
-	if(v%5 == 0 && v%3 == 0)
-	{
-		console.log(v); }
-	v  += 1; 
+// while(v<50)
+// {
+// 	if(v%5 == 0 && v%3 == 0)
+// 	{
+// 		console.log(v); }
+// 	v  += 1; 
+// }
+
+var answer = prompt("Are we there Yet");
+
+var count = 0;
+
+while(answer !== "yes" && answer !== "yeah"){
+
+var answer = prompt("Are we there Yet Again ??");
+
+count++;
+
 }
+
+alert("You Made It in " + count + " times");
